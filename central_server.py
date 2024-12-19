2c645fa1fd95e16094bc1f0cfe5e7dd62536b9a5
Charlie Cardenas <112025143+frggycc@users.noreply.github.com>
2024-12-18 20:13:13 -0800

    Included sample login
    Sends to client if login was successful

--- central_server.py
+++ central_server.py
@@ -4,32 +4,45 @@ import threading
 # List/Number of max connections
 MAX_CONNECTIONS = 10;
 
+HOST = '127.0.0.1'
+PORT = 8470
+
 # Might write to serperate file and use import
 def handle_client(client_socket, client_address):
-    print("Inside handle_client")
+    while True:
+        try:
+            # Receive password and username from client
+            log_data = client_socket.recv(1024).decode()
+            if not log_data:
+                break
+
+            # Split where char = ':'
+            username, password = log_data.split(":")
+
+            # Test back with echo
+            client_socket.send("Successful login".encode())
+
+        except Exception as e:
+            print(f"Error handling client: {e}")
+            break
+
+    client_socket.close()
 
 # Runs only if thread == main one; 
 if __name__ == "__main__":
-    host = '127.0.0.1'
-    port = 8470
 
     # Create socket and bind (IPv4 and TCP)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(host, port)
-
+    server_socket.bind((HOST, PORT))
     server_socket.listen(MAX_CONNECTIONS)
 
-    # Create list of clients that will connect
-    clients = []
-
     # Display status
-    print(f"Server listening on {host}:{port}")
+    print(f"Server listening on {HOST}:{PORT}")
 
-    # Eventually add authentication here!!!
+    # Eventually add authentication here
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connected to {client_address}")
-        clients.append(client_socket)
 
         # Create thread for each client connection
         # Might need to implement a way to avoid collisions
