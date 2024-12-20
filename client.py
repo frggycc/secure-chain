6183c357b21c35a76512f6a7693927c27a295458
Charlie Cardenas <charliecardenas.pro@gmail.com>
2024-12-20 15:53:07 -0800

    Password entry hidden; New input for creating new account

--- client.py
+++ client.py
@@ -4,17 +4,20 @@ import socket
 import bcrypt
 import ssl
 import sys
+import getpass
 
 # Get IP, port, and key from client
 SERVER_IP = str(sys.argv[1])
 SERVER_PORT = int(sys.argv[2])
 SERVER_KEY = str(sys.argv[3])
+CLIENT_CRT = "client.crt"
+CLIENT_KEY = "client.key"
 
 # # Encrypt with AES; Accept decoded and return encoded
-# def encrypt_message(message):
+# def encrypt_message(key, message):
 
 # # Decrypt messages; Accept encoded and returns decode plaintext
-# def decrypt_message(enc_message):
+# def decrypt_message(key, enc_message):
 
 # Verify message integrity
 def bcrypt_append(message):
@@ -37,7 +40,7 @@ def client_start():
     # Fails if client ctr not signed by CA
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     context.load_verify_locations(cafile="ca.crt")
-    context.load_cert_chain(certfile="client.crt", keyfile="client.key")
+    context.load_cert_chain(certfile=CLIENT_CRT, keyfile=CLIENT_KEY)
     
     # Set socket settings
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -67,8 +70,18 @@ def client_start():
 
                 # user's username and password to be validated
                 username = input("Username: ")
-                password = input("Password: ")
-                login_info = encrypt_message(SERVER_KEY, f"{username}:{password}")
+                password = getpass.getpass("Password: ")
+
+                # Ask for more info if creating account
+                if choice == "1":
+                    first = input("First Name: ")
+                    last = input("Last Name: ")
+                    email = input("Email: ")
+                    message = f"{username}:{password}:{first}:{last}:{email}"
+                else:
+                    message = f"{username}:{password}"
+
+                login_info = encrypt_message(SERVER_KEY, message)
                 secure_socket.send(login_info)
             
                 # Was the sign-in valid?
@@ -82,6 +95,7 @@ def client_start():
                 print(status)
 
         # CLEAR AND GO TO DIFFERENT MENU TO SEND IN ORDERS
+
 
         secure_socket.close()
         
